967ab21e4458e409a0de297a3394ab03585f7860
George Nijo <george.nijo8@gmail.com>
2026-03-25 14:52:48 -0400

    feat: add UI barrel exports and component showcase (#38)

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,4 +1,19 @@
+// Components
 export { Button } from './Button'
 export { GoldDivider } from './GoldDivider'
 export { SectionHeader } from './SectionHeader'
+
+// TODO: Uncomment when components are merged
+// export { Card } from './Card'
+// export { PageHero } from './PageHero'
+// export { ScrollReveal } from './ScrollReveal'
+
+// Types
+export type { ButtonProps } from './Button'
+export type { GoldDividerProps } from './GoldDivider'
 export type { SectionHeaderProps } from './SectionHeader'
+
+// TODO: Uncomment when components are merged
+// export type { CardProps } from './Card'
+// export type { PageHeroProps } from './PageHero'
+// export type { ScrollRevealProps } from './ScrollReveal'
