886bbefa2386468bc6a781a295ebab9abbfbc43b
George Nijo <george.nijo8@gmail.com>
2026-03-25 17:26:34 -0400

    feat: add ScrollReveal Framer Motion wrapper (#37) (#44)

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -4,6 +4,7 @@ export { Card } from './Card'
 export { GoldDivider } from './GoldDivider'
 export { JsonLd } from './JsonLd'
 export { PageHero } from './PageHero'
+export { ScrollReveal } from './ScrollReveal'
 export { SectionHeader } from './SectionHeader'
 
 // Types
@@ -11,4 +12,5 @@ export type { ButtonProps } from './Button'
 export type { CardProps, CardSectionProps } from './Card'
 export type { GoldDividerProps } from './GoldDivider'
 export type { PageHeroProps } from './PageHero'
+export type { ScrollRevealProps, ScrollRevealItemProps } from './ScrollReveal'
 export type { SectionHeaderProps } from './SectionHeader'
