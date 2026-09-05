3a3789b1e92ce94085b0fcf77995271f6fa32e21
George Nijo <george.nijo8@gmail.com>
2026-09-05 19:02:18 -0400

    Fix CI browser manifest race (#305)

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
   webServer: process.env.BASE_URL
     ? undefined
     : {
-        command: 'npm run dev',
+        command: process.env.CI ? 'npm run build && npm run start' : 'npm run dev',
         url: 'http://localhost:3000',
         reuseExistingServer: !process.env.CI,
         timeout: 120_000,
